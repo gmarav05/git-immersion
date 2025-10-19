14aa62fe25973ca02ecf189dd52f4dc1a56694ff
Aravind <gmarav005@gmail.com>
2025-10-19 20:34:14 +0530

    Oops, we didn't want this commit

--- work/hello/hello.rb
+++ work/hello/hello.rb
@@ -1,10 +1,4 @@
-# puts "Hello, World"
-
-# Default is "World"
+# This is an unwanted but committed change
 name = ARGV.first || "World"
 
 puts "Hello, #{name}!"
-
-"Test GPG key Sign"
-"Lab 16"
-"Test Sign now"
